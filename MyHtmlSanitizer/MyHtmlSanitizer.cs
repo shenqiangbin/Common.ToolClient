a3acc19734a5d945d4f24cfbc6dac01c1b8783b2
sqb <shenqiangbin@163.com>
2017-11-13 15:31:45 +0800

    xss 漏洞防备

---
+++ MyHtmlSanitizer/MyHtmlSanitizer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyHtmlSanitizer
+{
+    /// <summary>
+    /// 使用时，注意将app.config中的配置copy到引用的程序中。
+    /// </summary>
+    public class MyHtmlSanitizer
+    {
+        public static Dictionary<string, object> Filter(Dictionary<string, object> dic)
+        {
+            Dictionary<string, object> infoListCopy = new Dictionary<string, object>();
+            var htmlSan = new Ganss.XSS.HtmlSanitizer();
+            foreach (var item in dic.Keys)
+            {
+                infoListCopy.Add(item, htmlSan.Sanitize(dic[item].ToString()));
+            }
+            return infoListCopy;
+        }
+
+        public static string Sanitize(string content)
+        {
+            return new Ganss.XSS.HtmlSanitizer().Sanitize(content);
+        }
+    }
+}
